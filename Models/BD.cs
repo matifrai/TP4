d11e10431b2fe4fbd7a29a3623738687217d50f3
SEBASTIÁN KO <49702048@ort>
2026-07-01 09:00:14 -0300

    faltan 3 metodos de la clase BD

--- Models/BD.cs
+++ Models/BD.cs
@@ -5,4 +5,24 @@ using TP4.Models;
 public class BD{
     private string _connectionString = @"Server=localhost; 
     DataBase=TP04; Integrated Security=True;TrustServerCertificate=True;";
+
+
+    public List<Jugadores> ObtenerTodos(){
+        List<Jugadores> jugadores = new List<Jugadores>();
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+            string query = "SELECT * FROM Jugadores";
+            jugadores = connection.Query<Jugadores>(query).ToList();
+        }
+        return jugadores;
+    }
+
+    public Jugadores obtenerPorId(int ID){
+        Jugadores jugador = null;
+        string query = "SELECT * FROM Jugadores WHERE Numero = @pID";
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+            jugador = connection.QueryFirstOrDefault<Jugadores>(query, new {pID = ID})
+        }
+        return jugador;
+
+    } 
 }
--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -3,7 +3,7 @@ namespace TP4.Models;
 public class Jugadores{
     public string nombre {get;set;}
     public string club{get;set;}
-    public int id{get;set;}
+    public int Numero{get;set;}
     public int idSelecciones{get;set;}
 
 }
