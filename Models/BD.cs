6d04eb90dda762b4a761dcec3548b88262608bad
SEBASTIÁN KO <49702048@ort>
2026-07-02 08:55:59 -0300

    clase BD(faltan 2 metodos)

--- Models/BD.cs
+++ Models/BD.cs
@@ -23,6 +23,33 @@ public class BD{
             jugador = connection.QueryFirstOrDefault<Jugadores>(query, new {pID = ID})
         }
         return jugador;
-
     } 
+
+    public List<Jugadores> abrirSobre (){
+        List<Jugadores> jugadores = new List<Jugadores>();
+        string query = "SELECT * FROM Jugadores";
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+            jugadores = connection.Query<Jugadores>(query).ToList();
+        }
+        List<Jugadores> sobre = new List<Jugadores>();
+        for(int i = 0; i<5; i++;){
+            Random aleatorio = new Random();
+            int numero = aleatorio.Next(0, 20);
+            sobre.Add(jugadores[numero]);
+        }
+        return sobre;
+    }
+
+    public int VerificarFiguritas (int id){
+        string query = "SELECT * FROM FiguritasPegadas WHERE IdFigurita = @idFigurit";
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+    }
+    return id;
+    }
+
+    public void PegarFiguritas(int idJugador){
+        string query = "INSERT INTO FiguritaUsuario (IdFigurita, IdAlbum) VALUES (@idFigurita, @idAlbum)";
+        using(SqlConnection connection = new SqlConnection(_connectionString)){
+    }
+
 }
